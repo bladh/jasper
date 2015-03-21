4b7f2202fe0104049c30d325616b18cdc4e4ca8a
Joakim Saario <s@xyr.se>
2015-03-21 23:37:32 +0100

    connect to real server

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
-import ("net"
-        "fmt"
+import (
+	"fmt"
+	"net"
 )
 
 func main() {
-   conn, err := net.Dial("tcp", "localhost:6667")
-   if err != nil {
-      fmt.Printf("Couldn't connect to host")
-   } else {
-      fmt.Fprintf(conn, "USER alpha-centauri * 0 :alpha-centauri\n")
-      fmt.Fprintf(conn, "NICK alpha-centauri")
-      fmt.Fprintf(conn, "JOIN #jasper")
-   }
+	conn, err := net.Dial("tcp", "irc.underworld.no:6667")
+	if err != nil {
+		fmt.Printf("Couldn't connect to host")
+	} else {
+		fmt.Fprintf(conn, "USER alpha-centauri * 0 :alpha-centauri\n")
+		fmt.Fprintf(conn, "NICK alpha-centauri\n")
+		fmt.Fprintf(conn, "JOIN #jasper\n")
+	}
+
+	for {
+	}
 }
